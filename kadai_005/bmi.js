a5217ce3f42bf07badb95e05c8c6c6d0971bdbe2
daiyahome <honbu@daiyahome.jp>
2024-09-04 14:58:23 +0900

    bmi.jsを編集

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,7 @@
 // 変数の宣言・値の代入
 let bodyWeight = 68;
 let bodyHight = 1.7;
+let bmi = bodyWeight / (bodyHight * bodyHight);
 
 // コンソールへの出力
-console.log(bodyWeight / (bodyHight * bodyHight));
+console.log(bmi);
