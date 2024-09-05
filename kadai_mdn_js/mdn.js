938fb12536c2f3dc005f0092d222434cf8b03ebe
daiyahome <honbu@daiyahome.jp>
2024-09-05 15:37:31 +0900

    mdn.jsを編集

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const today = new Date();
 const year = today.getFullYear();
-const month = today.getMonth();
+const month = today.getMonth() +1;
 const date = today.getDate();
 
 const formatDate = year + '年' + month + '月' + date + '日';
